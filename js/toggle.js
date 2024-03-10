c7715b292c09e5f7c1ae637b177b1ad9d66dd805
AsadTufail <42470754+AsadTufail@users.noreply.github.com>
2024-03-10 14:06:29 +0500

    Dark mode

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+const BUTTON = document.querySelector("button");
+
+const TOGGLE = () => {
+  const IS_PRESSED = BUTTON.matches("[aria-pressed=true]");
+    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
+  BUTTON.setAttribute("aria-pressed", IS_PRESSED ? false : true);
+};
+
+BUTTON.addEventListener("click", TOGGLE);
